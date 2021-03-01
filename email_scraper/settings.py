05f95c11829753df2f0e8b0b2db3e2e0f13cb243
Ubuntu <ubuntu@ip-172-31-26-211.us-east-2.compute.internal>
2021-03-01 12:08:50 +0000

    j

--- email_scraper/settings.py
+++ email_scraper/settings.py
@@ -119,7 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'tp_scraper')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'tp_scraper/static')
+
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
